0bf376a80481dee6dfe029f168c003584561169b
Brahvim <69293652+Brahvim@users.noreply.github.com>
2023-02-20 12:43:13 +0530

    `AlTypedBuffer.java`. :D!

--- src/com/brahvim/nerd/openal/AlTypedBuffer.java
+++ src/com/brahvim/nerd/openal/AlTypedBuffer.java
@@ -7,21 +7,22 @@ import java.nio.IntBuffer;
 import java.nio.ShortBuffer;
 
 import org.lwjgl.openal.AL11;
+import org.lwjgl.openal.ALC11;
 import org.lwjgl.system.MemoryStack;
 
-public class AlBuffer {
+/* `package` */ abstract class AlTypedBuffer<T> {
 	private int bufId;
 	private NerdAl alInst;
 
 	// region Constructors.
-	public AlBuffer(NerdAl p_alInst) {
+	public AlTypedBuffer(NerdAl p_alInst) {
 		this.alInst = p_alInst;
 
 		this.bufId = AL11.alGenBuffers();
 		this.alInst.checkForErrors();
 	}
 
-	public AlBuffer(NerdAl p_alInst, Buffer p_data) {
+	public AlTypedBuffer(NerdAl p_alInst, Buffer p_data) {
 		this.alInst = p_alInst;
 
 		this.bufId = AL11.alGenBuffers();
@@ -45,24 +46,51 @@ public class AlBuffer {
 		return retVal.get();
 	}
 
-	public void getIntegerData() {
-		IntBuffer retVal = IntBuffer.allocate(this.getSize());
-		AL11.alBufferiv(bufId, bufId, null);
+	public int getBits() {
+		// Not using `MemoryStack` to allocate here! This stuff is being returned!
+		// ...but WAIT, it works..?!
+		MemoryStack.stackPush();
+		IntBuffer retVal = MemoryStack.stackMallocInt(1);
+		AL11.alGetBufferiv(this.bufId, AL11.AL_BITS, retVal);
+		MemoryStack.stackPop();
+
+		return retVal.get();
 	}
 
-	public void getFloatData() {
-		AL11.
+	public int getChannels() {
+		// Not using `MemoryStack` to allocate here! This stuff is being returned!
+		// ...but WAIT, it works..?!
+		MemoryStack.stackPush();
+		IntBuffer retVal = MemoryStack.stackMallocInt(1);
+		AL11.alGetBufferiv(this.bufId, AL11.AL_CHANNELS, retVal);
+		MemoryStack.stackPop();
+
+		return retVal.get();
 	}
+
+	public int getSampleRate() {
+		MemoryStack.stackPush();
+		IntBuffer sampleRateBuffer = MemoryStack.stackMallocInt(1);
+		AL11.alBufferiv(this.bufId, ALC11.ALC_FREQUENCY, sampleRateBuffer);
+		MemoryStack.stackPop();
+
+		return sampleRateBuffer.get();
+	}
+
+	public abstract T[] getData();
 	// endregion
 
 	// region Setters.
-	public void setBits() {
+	public void setBits(int p_bits) {
+		AL11.alBufferi(this.bufId, AL11.AL_BITS, p_bits);
 	}
 
-	public void setFreq() {
+	public void setSampleRate(int p_sampleRate) {
+		AL11.alBufferi(this.bufId, AL11.AL_FREQUENCY, p_sampleRate);
 	}
 
-	public void setChannels() {
+	public void setChannels(int p_channels) {
+		AL11.alBufferi(this.bufId, AL11.AL_CHANNELS, p_channels);
 	}
 
 	// region `setData()` overloads.
