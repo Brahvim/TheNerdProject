e894e4baf1e2926ba9c5cac7ebc3359668d1209a
Brahvim <69293652+Brahvim@users.noreply.github.com>
2023-04-06 01:26:31 +0530

    Woof! I better stop for now. TCP later, people!

--- src/com/brahvim/nerd/io/net/TcpServer.java
+++ src/com/brahvim/nerd/io/net/TcpServer.java
@@ -2,17 +2,27 @@ package com.brahvim.nerd.io.net;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class TcpServer {
 
+	// region Fields.
 	private ServerSocket socket;
+	private Thread connsThread, commsThread;
+	private ArrayList<Socket> clients = new ArrayList<>();
+	// endregion
 
+	// region Construction.
 	public TcpServer() {
 		try {
 			this.socket = new ServerSocket();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		this.delegatedConstruction();
 	}
 
 	public TcpServer(final int p_port) {
@@ -21,6 +31,8 @@ public class TcpServer {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		this.delegatedConstruction();
 	}
 
 	public TcpServer(final int p_port, final int p_maxConnReqsAtOnce) {
@@ -29,6 +41,35 @@ public class TcpServer {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		this.delegatedConstruction();
+	}
+
+	private void delegatedConstruction() {
+		this.connsThread = new Thread(() -> {
+			try {
+				this.clients.add(this.socket.accept());
+			} catch (IOException e) {
+				// e.printStackTrace();
+			}
+		});
+
+		// this.commsThread = new Thread(() -> { });
+	}
+	// endregion
+
+	public TcpServer send(final Socket p_client, final String p_bytes) {
+		this.send(p_client, p_bytes.getBytes(StandardCharsets.UTF_8));
+		return this;
+	}
+
+	public TcpServer send(final Socket p_client, final byte[] p_bytes) {
+		try {
+			p_client.getOutputStream().write(p_bytes);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return this;
 	}
 
 }
