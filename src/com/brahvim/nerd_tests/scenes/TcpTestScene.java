8750b7c0fe5089f4abd61a9816bf118a914184a0
Brahvim <69293652+Brahvim@users.noreply.github.com>
2023-04-09 20:29:58 +0530

    Minor refactoring for major logic!

--- src/com/brahvim/nerd_tests/scenes/TcpTestScene.java
+++ src/com/brahvim/nerd_tests/scenes/TcpTestScene.java
@@ -1,11 +1,8 @@
 package com.brahvim.nerd_tests.scenes;
 
-import java.util.HashSet;
 import java.util.function.Consumer;
-import java.util.function.Function;
 
 import com.brahvim.nerd.io.ByteSerial;
-import com.brahvim.nerd.io.net.tcp.NerdAbstractTcpClient;
 import com.brahvim.nerd.io.net.tcp.NerdTcpClient;
 import com.brahvim.nerd.io.net.tcp.NerdTcpServer;
 import com.brahvim.nerd.io.net.tcp.NerdTcpServer.NerdClientSentTcpPacket;
@@ -35,49 +32,27 @@ public class TcpTestScene extends NerdScene {
 
 	private enum Response {
 		ALLOWED(),
-		REJECTED(),
+		// REJECTED(),
 		SERVED_FOOD(),
 	}
 	// endregion
 
-	private class RestaurantApi implements Consumer<NerdClientSentTcpPacket>,
-			Function<NerdAbstractTcpClient, Consumer<NerdClientSentTcpPacket>> {
+	private class RestaurantApi implements Consumer<NerdClientSentTcpPacket> {
 
-		// Message callback:
 		@Override
 		public void accept(final NerdClientSentTcpPacket p_packet) {
+			// Get the client's message:
 			final Query message = (Query) ByteSerial.fromBytes(p_packet.getData());
 			final NerdTcpServer.NerdTcpServerClient client = p_packet.getSender();
-			Response response = null;
-
-			// System.out.println(
-			// "`TcpTestScene.RestaurantApi::accept()` (Server message loop)");
+			Response response = null; // In here, we store our response!
 
 			switch (message) {
-				case ORDER_FOOD -> {
-					response = Response.SERVED_FOOD;
-					System.out.println("The server served food!");
-				}
+				// If food is ordered, we serve.
+				case ORDER_FOOD -> response = Response.SERVED_FOOD;
 			}
 
+			// Send the response over!
 			client.send(ByteSerial.toBytes(response));
-		}
-
-		// New connection callback:
-		@Override
-		public Consumer<NerdClientSentTcpPacket> apply(final NerdAbstractTcpClient p_client) {
-			final boolean clientAccepted = SKETCH.random(1) < 0.5f;
-
-			if (clientAccepted) {
-				p_client.send(ByteSerial.toBytes(Response.ALLOWED));
-				System.out.println("Ayy! A new client joined! Info: "
-						+ p_client.getSocket().toString());
-			} else
-				System.out.println("The server rejected a connection.");
-
-			// API Feature!: returning `null` rejects the connection,
-			// Can also do it here, but we have this now!
-			return clientAccepted ? this : null;
 		}
 
 	}
@@ -102,33 +77,53 @@ public class TcpTestScene extends NerdScene {
 	}
 
 	private void netTest() {
-		final NerdTcpServer server = new NerdTcpServer(8080, new RestaurantApi());
-		final HashSet<NerdTcpClient> clients = new HashSet<>();
+		// Start a TCP server on the given port and check for clients to join!:
+		final NerdTcpServer server = new NerdTcpServer(8080,
+				// `c` holds the client object that wishes to join.
+				c -> {
+					// ...Let clients be rejected by chance:
+					final boolean clientAccepted = SKETCH.random(1) < 0.5f;
 
-		System.out.println();
+					if (clientAccepted) {
+						// This client got accepted - hooray! Tell it!:
+						c.send(ByteSerial.toBytes(Response.ALLOWED));
+						System.out.println("Ayy! A new client joined! Info: "
+								+ c.getSocket().toString());
+					} else // Tell us that it got rejected otherwise.
+						System.out.println("The server rejected a connection.");
+
+					// Returning `null` or calling `c.disconnect()` should disconnect.
+					// If we accept this client, we return a listener to listen to its messages!:
+					return clientAccepted ? new RestaurantApi() : null;
+				});
+
+		// Now, we start 5 clients to connect to the server!
 
 		for (int i = 0; i < 5; i++)
-			clients.add(new NerdTcpClient("127.0.0.1", 8080, (p) -> {
-				final Response message = (Response) ByteSerial.fromBytes(p.getData());
-				Query response = null;
+			new NerdTcpClient("127.0.0.1", 8080,
+					// `p` holds the packet of data received!:
+					p -> {
+						// Get the server's message,
+						final Response message = (Response) ByteSerial.fromBytes(p.getData());
+						Query response = null; // We'll store our response here.
 
-				switch (message) {
-					case ALLOWED -> {
-						response = Query.ORDER_FOOD;
-						System.out.println("Asked the server for food!");
-					}
-					case REJECTED, SERVED_FOOD -> {
-						p.getReceivingClient().disconnect();
-						System.out.println("Received the food, disconnecting now!");
-					}
-				}
+						switch (message) {
+							case ALLOWED -> { // If the server happily allows us, we order!:
+								response = Query.ORDER_FOOD;
+								System.out.println("Asked the server for food!");
+							}
+							case SERVED_FOOD -> { // If we're served food, we happily take it and go!:
+								p.getReceivingClient().disconnect();
+								System.out.println("Received the food, disconnecting now!");
+							}
+						}
 
-				if (response != null)
-					p.getReceivingClient().send(ByteSerial.toBytes(response));
-			}));
+						if (response != null)
+							p.getReceivingClient().send(ByteSerial.toBytes(response));
+					});
 
 		SKETCH.delay(50);
-		System.out.println("Shutting server down...");
+		System.out.println("Great day! The restaurant served well.");
 		server.shutdown();
 		SKETCH.exit();
 	}
