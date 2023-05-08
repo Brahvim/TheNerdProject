738693e6adf5dbd73c1a3b97581cbd6533a57e7c
Brahvim <69293652+Brahvim@users.noreply.github.com>
2023-05-08 12:52:23 +0530

    Don't mind me! Just committing stuff.

--- src/com/brahvim/nerd/framework/ecs/NerdEcsSystemManager.java
+++ src/com/brahvim/nerd/framework/ecs/NerdEcsSystemManager.java
@@ -1,5 +1,13 @@
 package com.brahvim.nerd.framework.ecs;
 
+import java.util.LinkedList;
+
 public class NerdEcsSystemManager {
+
+    private final LinkedList<NerdEcsSystem> SYSTEMS;
+
+    public NerdEcsSystemManager() {
+        this.SYSTEMS = new LinkedList<>();
+    }
 
 }
